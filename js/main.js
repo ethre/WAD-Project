9d77ed9d0ebfc139ae83cf003d957f74aa3133da
ethre <ethre@hotmail.com>
2015-12-02 20:40:22 +0000

    (づ$‿‿$)づ

--- js/main.js
+++ js/main.js
@@ -4,3 +4,44 @@ jQuery(document).ready(function($){
 		if($(event.target).is('.cd-main-nav')) $(this).children('ul').toggleClass('is-visible');
 	});
 });
+
+function preloader(immune, background, color) {
+  $("body").prepend('<div class="preloader"><span class="loading-bar"></span><i class="radial-loader"></i></div>');
+
+  if (immune == true) {
+    $("body > div.preloader").addClass('immune');
+  }
+
+  if (background == 'white') {
+    $("body > div.preloader").addClass('white');
+  }
+  
+  else if (background == 'black') {
+    $("body > div.preloader").addClass('black');
+  }
+
+  if (color == 'red') {
+    $("body > div.preloader span.loading-bar").addClass('red-colored');
+    $("body > div.preloader i.radial-loader").addClass('red-colored');
+  } else if (color == 'blue') {
+    $("body > div.preloader span.loading-bar").addClass('blue-colored');
+    $("body > div.preloader i.radial-loader").addClass('blue-colored');
+  } else if (color == 'green') {
+    $("body > div.preloader span.loading-bar").addClass('green-colored');
+    $("body > div.preloader i.radial-loader").addClass('green-colored');
+  } else if (color == 'yellow') {
+    $("body > div.preloader span.loading-bar").addClass('yellow-colored');
+    $("body > div.preloader i.radial-loader").addClass('yellow-colored');
+  }
+  $(window).load(function() {
+    setTimeout(function() {
+      $('.preloader').fadeOut(2000);
+    }, 2000);
+    setTimeout(function() {
+      $('.preloader').remove();
+    }, 3000);
+    
+  })
+};
+
+preloader(true, 'black', 'red');
